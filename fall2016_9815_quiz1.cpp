68d559290fdd98847df86a9bba04e3c1346b3d9e
liulanchun <liulanchun@yahoo.com>
2016-11-04 13:40:50 -0400

    quiz commit

---
+++ fall2016_9815_quiz1.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : fall2016_9815_quiz1.cpp
+// Author      : tom
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	int a = 10;
+	int b = 20;
+
+	auto c = a+b; // c will be int
+	cout << c;
+
+	return 0;
+}
